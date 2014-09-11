ca200e9266aa0bc9efc4c786c044398f5df94368
shahnawazdaanish <shahnawazdaanish@gmail.com>
2014-09-11 16:29:26 +0600

    testing js
    works or not

--- screensize.js
+++ screensize.js
@@ -1,6 +1,1 @@
-window.onresize = displayWindowSize;
-window.onload = displayWindowSize;
-function displayWindowSize() {
-    // your size calculation code here
-    document.getElementById("dimensions").innerHTML = myWidth + "x" + myHeight;
-};
+alert("JS Works");
